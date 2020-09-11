c6260fe60d319271de32fe1fe0fa37ac66aa3e29
Dhanya156 <dhanyavirupaksha@gmail.com>
2020-09-11 10:06:50 -0700

    Added:User Validation of User Email

--- UserRegistrationProblem.sh
+++ UserRegistrationProblem.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 echo "WELCOME TO USER REGISTRATION PROBLEM"
 
-read -p "Enter the last name " Lname
-pattern="^[A-Z]+[a-z]{3,}"
-if [[ $Lname =~ $pattern ]]
+read -p "Enter the Users valid email " email
+pattern="^[a-z]+[0-9]*([-_+.][0-9a-z]+)*@[0-9a-z]+[.][a-z]{2,4}([,]{1})*([.][a-z]{2})*$"
+if [[ $email =~ $pattern ]]
 then
         echo valid
 else
