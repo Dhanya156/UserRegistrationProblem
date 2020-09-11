15d99e401131fc2c3e080ce96dbaa2c1b25b6198
Dhanya156 <dhanyavirupaksha@gmail.com>
2020-09-11 10:18:49 -0700

    Added:User Password Validation Rule 3

--- UserRegistrationProblem.sh
+++ UserRegistrationProblem.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 echo "WELCOME TO USER REGISTRATION PROBLEM"
 
+
 read -p "Enter the User password " passwd
 pattern=$(($(tr -d '[[:alnum:]]' <<<$passwd | wc -m)-1))
-if [[ ${#passwd} -ge 8 && $passwd =~ [[:upper:]] ]]
+if [[ ${#passwd} -ge 8 && $passwd =~ [[:upper:]] && $passwd =~ [[:digit:]] ]]
 then
         echo "valid password"
 else
