1e6788c250f73182d40a5dc67e1eee5d9aee371d
Dhanya156 <dhanyavirupaksha@gmail.com>
2020-09-11 10:23:22 -0700

    Added:User Password Validation Rule 4

--- UserRegistrationProblem.sh
+++ UserRegistrationProblem.sh
@@ -4,7 +4,7 @@ echo "WELCOME TO USER REGISTRATION PROBLEM"
 
 read -p "Enter the User password " passwd
 pattern=$(($(tr -d '[[:alnum:]]' <<<$passwd | wc -m)-1))
-if [[ ${#passwd} -ge 8 && $passwd =~ [[:upper:]] && $passwd =~ [[:digit:]] ]]
+if [[ ${#passwd} -ge 8 && $passwd =~ [[:upper:]] && $passwd =~ [[:digit:]] && $passwd =~ 1 ]]
 then
         echo "valid password"
 else
