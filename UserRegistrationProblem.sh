28b740c3bbb0f0e3cbd4b31d7b56945f47da8737
Dhanya156 <dhanyavirupaksha@gmail.com>
2020-09-11 10:01:30 -0700

    Added:User Validation of FirstName

--- UserRegistrationProblem.sh
+++ UserRegistrationProblem.sh
@@ -1,2 +1,11 @@
 #! /bin/bash
 echo "WELCOME TO USER REGISTRATION PROBLEM"
+
+read -p "Enter the firstName " fname
+pattern="^[A-Z]+[a-z]{3,}"
+if [[ $fname =~ $pattern ]]
+then
+        echo valid
+else
+        echo invalid
+fi
